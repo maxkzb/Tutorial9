9d2935b81655c6a5e21dd85091198a135c688cad
Maks Kozub <maks.kozub@gmail.com>
2025-05-11 19:26:47 +0200

    Initial commit

---
+++ Tutorial9/Controllers/WarehouseController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial9.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WarehouseController : ControllerBase
+{
+    
+}
---
+++ Tutorial9/Model/ProductWarehouseDTO.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Tutorial9.Model;
+
+public class ProductWarehouseDTO
+{
+    public int IdProduct { get; set; }
+    public int IdWarehouse { get; set; }
+    public int Amount { get; set; }
+    public JSType.Date Date { get; set; }
+}
